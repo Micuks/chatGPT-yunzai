70236192d6b1f624e8e13a0943c324d23a3c5ef6
micuks <sdkjwql@gmail.com>
2023-11-25 16:24:31 +0800

    feat: refactored question

--- components/question.js
+++ components/question.js
@@ -1,15 +1,100 @@
+import { deprecate } from "util";
+import russianJoke from "./russianJoke";
+
 export default class Question {
-  constructor(question, sender) {
-    this.question = question;
-    this.sender = sender;
+  constructor(msg, questionType = undefined, e) {
+    this.e = e;
+    this.sender = e.sender;
+    this.msg = msg;
+    // Get questionBody and questionType{ChatGPT, Bard}
+    this.parserQuestion();
+    this.metaInfo = this.getMetaInfo();
+
+    // Deprecated
     this.prevChat = undefined;
   }
 
+  questionType = {
+    ChatGPT: "ChatGPT",
+    Bard: "Bard",
+  };
+
+  parseQuestion = () => {
+    let gptReg = /^(\?|!|4|\/gpt4|gpt4|gpt|\/gpt)(.*)$/;
+    let bardReg = /^(B|bard|\/bard)(.*)$/;
+
+    let msg = this.e.msg;
+
+    let questionBody = "";
+    let questionType = this.questionType.ChatGPT;
+
+    if (gptReg.test(msg)) {
+      questionBody = gptReg.exec(msg)[2];
+      questionType = this.questionType.ChatGPT;
+    } else if (bardReg.test(msg)) {
+      questionBody = bardReg.exec(msg)[2];
+      questionType = this.questionType.Bard;
+    } else {
+      questionBody = russianJoke.russianJokePrompt;
+    }
+
+    this.questionBody = questionBody;
+    this.questionType = questionType;
+  };
+
+  getMetaInfo = () => {
+    let metaInfo = redis.get(`CHATGPT:${this.sender.user_id}`);
+    if (!metaInfo) {
+      metaInfo = this.newMetaInfo();
+    }
+    try {
+      metaInfo = JSON.parse(metaInfo);
+    } catch (err) {
+      metaInfo = this.newMetaInfo();
+    }
+
+    return metaInfo;
+  };
+
+  setMetaInfo = (metaInfo) => {
+    try {
+      redis.set(`CHATGPT:${this.sender.user_id}`, JSON.stringify(metaInfo));
+    } catch (err) {
+      console.error(
+        `Failed to set Meta Info for user ${this.sender.user_id}: ${err}`
+      );
+      return false;
+    }
+
+    return true;
+  };
+
+  newMetaInfo = () => {
+    let ctime = new Date();
+    return {
+      bardInfo: {
+        ctime: ctime,
+        utime: ctime,
+        sender: this.sender,
+        parentMessageId: undefined,
+        conversationId: this.sender.user_id,
+      },
+      chatGptInfo: {
+        ctime: ctime,
+        utime: ctime,
+        sender: this.sender,
+        parentMessageId: undefined,
+        conversationId: this.sender.user_id,
+      },
+    };
+  };
+
+  @deprecate
   bardGetOrCreatePrevChat = async () => {
     let prevChat = await redis.get(`CHATGPT:BARD_CHATS:${this.sender.user_id}`);
     if (!prevChat) {
       logger.info(
-        `No previous bard chat for ${this.sender.nickname}[${this.sender.user_id}]`,
+        `No previous bard chat for ${this.sender.nickname}[${this.sender.user_id}]`
       );
       prevChat = this.createNewPrevChat("Bard");
     } else {
@@ -24,24 +109,25 @@ export default class Question {
     const timeOut = 600;
     if (timeElapsed > timeOut) {
       logger.info(
-        `Your chat expired: ${timeElapsed} seconds passed after your last active time.`,
+        `Your chat expired: ${timeElapsed} seconds passed after your last active time.`
       );
       prevChat = this.createNewPrevChat("Bard");
     }
 
     await redis.set(
       `CHATGPT:BARD_CHATS:${this.sender.user_id}`,
-      JSON.stringify(prevChat),
+      JSON.stringify(prevChat)
     );
 
     return prevChat;
   };
 
+  @deprecate
   gptGetOrCreatePrevChat = async () => {
     let prevChat = await redis.get(`CHATGPT:CHATS:${this.sender.user_id}`);
     if (!prevChat) {
       logger.info(
-        `No previous chats of ${this.sender.nickname}[${this.sender.user_id}]`,
+        `No previous chats of ${this.sender.nickname}[${this.sender.user_id}]`
       );
       prevChat = this.createNewPrevChat();
     } else {
@@ -62,7 +148,7 @@ export default class Question {
 
     await redis.set(
       `CHATGPT:CHATS:${this.sender.user_id}`,
-      JSON.stringify(prevChat),
+      JSON.stringify(prevChat)
     );
 
     return prevChat;
@@ -78,6 +164,12 @@ export default class Question {
     }
   }
 
+  /**
+   *
+   * @param {string} model model name, ChatGPT or Bard
+   * @returns new PrevChat Object
+   */
+  @deprecate
   createNewPrevChat = (model = "ChatGPT") => {
     const ctime = new Date();
     return {
@@ -85,7 +177,7 @@ export default class Question {
       count: 0,
       ctime: ctime,
       utime: ctime,
-      conversationId: (model == "Bard") ? this.sender.user_id : undefined,
+      conversationId: model == "Bard" ? this.sender.user_id : undefined,
       parentMessageId: undefined,
     };
   };
---
+++ components/russianJoke.js
@@ -0,0 +1,4 @@
+export default RussianJoke = {
+  russianJokePrompt:
+    "结合上下文, 讲一个苏联笑话风格的笑话, 要幽默风趣. 只需要输出笑话内容, 不需要给出其他信息, 例如对笑话的评价等.",
+};
