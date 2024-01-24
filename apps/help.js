4f22f09af2fbac09074839520cc735ec2181e851
Micuks <sdkjwql@gmail.com>
2024-01-24 15:55:04 +0800

    chore: update help info

--- apps/help.js
+++ apps/help.js
@@ -19,18 +19,10 @@ export class help extends plugin {
 
   async help(e) {
     await this.reply(
-      `Ask question: ?Question\n` +
-        `Continuous Chatting: !Question\n` +
-        (Config.useGpt4 ? `**Chat with GPT-4**: 4Question\n` : ``) +
-        (Config.useBard ? `**Chat with Bard**: BQuestion\n` : ``) +
-        `Get chats statistics: #聊天列表\n` +
-        `Destroy your chat: #结束对话\n\n` +
-        `询问问题: ?问题\n` +
-        `连续对话: !问题\n` +
-        (Config.useGpt4 ? `**和GPT-4交谈**: 4问题\n` : ``) +
+        `询问问题: ?|!|gpt问题\n` +
+        (Config.useGpt4 ? `**和GPT-4交谈**: gpt4|4问题\n` : ``) +
         (Config.useBard ? `**和Google Bard交谈**: B问题\n` : ``) +
-        `获取聊天统计[管理员]: #聊天列表\n` +
-        `删除当前对话: #结束对话`
+        `十分钟内无交流会重置对话`
     );
   }
 }
